0a1da75530317fa88b956a9558d2d521ce4d2d61
mdbabaylan <matmatmark@gmail.com>
2023-11-17 22:28:48 +0800

    tinify shrink and dl shrinked image working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-file-input v-model="file" label="Choose an image" prepend-icon="mdi-camera"
+          @change="compressAndUploadImage"></v-file-input>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-img :src="uploadedImageUrl"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
+// import AWS from 'aws-sdk';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+// Create an axios instance without the Content-Type header
+const instance = axios.create();
+
+instance.defaults.headers.common = {};
+
+export default {
+  data() {
+    return {
+      file: null,
+      uploadedImageUrl: null
+    };
+  },
+  methods: {
+    async compressAndUploadImage() {
+      if (!this.file) return;
+
+      // Step 1: Compress the image using TinyPNG
+      const compressedImage = await this.compressImage(this.file);
+
+      // Step 2: Upload the compressed image to S3
+      if (compressedImage) {
+        this.uploadToS3(compressedImage);
+      }
+    },
+
+    async compressImage(file) {
+      // const formData = new FormData();
+      // formData.append('file', file);
+      console.log(file.type);
+
+      try {
+
+        //peskt CORS error, refer to vue.config.js
+        const response = await instance.post('/shrink', file, {
+          'Content-Type': file.type // 'image/png' or 'image/jpeg', for example
+        });
+
+        if (response.data && response.data.output && response.data.output.url) {
+          // Download the compressed image
+
+          const tinyUrl = response.data.output.url; // The original URL from TinyPNG
+
+          //remove 'https://api.tinify.com' to the proxy takes over
+          const localUrl = tinyUrl.replace('https://api.tinify.com/output/', '/output/');
+          console.log(localUrl);
+          const compressedImageResponse = await instance.get(localUrl, {
+            responseType: 'blob'
+          });
+          return new File([compressedImageResponse.data], file.name, { type: file.type });
+        }
+      } catch (error) {
+        console.error('Error compressing image:', error);
+      }
+      return null;
+    },
+
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    proxy: {
+      '/shrink': {
+        target: 'https://api.tinify.com',
+        changeOrigin: true,
+        pathRewrite: { '^/shrink': '/shrink' },
+        secure: false,
+        headers: {
+          Authorization: `Basic ${Buffer.from('api:DrfWrkcr0pPnR20dQ5pDq8q7Tys1Gf1H').toString('base64')}`,
+        }
+      },
+      '/output': {
+        target: 'https://api.tinify.com',
+        changeOrigin: true,
+        // pathRewrite: { '^/output': '/output' },
+        secure: false,
+        headers: {
+          Authorization: `Basic ${Buffer.from('api:DrfWrkcr0pPnR20dQ5pDq8q7Tys1Gf1H').toString('base64')}`,
+        }
+      },  
+    }
+  }
 })
